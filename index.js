4ba25fa4bdf3dc1304efdc1b6ed8d469c2a423fc
samim0773 <samimaktar0773@gmail.com>
2026-04-15 08:11:09 +0530

    make some improve in server

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const http = require('node:http');
 
 const server = http.createServer((req, res) => {
-    console.log('server is running ');
-    res.writeHead(200);
-    res.end('Thanks for visiting my server')
+    console.log('incomming request...');
+
+    switch (req.url) {
+        case '/':
+            res.writeHead(200)
+            return res.end('Home page')
+        case '/about':
+            res.writeHead(200)
+            return res.end('I am a software engineer')
+        case '/contact-us':
+            res.writeHead(200);
+            return res.end('Connect me at: samim@test.com')
+        default:
+            res.writeHead(404)
+            return res.end('not found.....')
+    }
 
 });
 
